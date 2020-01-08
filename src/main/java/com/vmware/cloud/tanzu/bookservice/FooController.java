f9b928cab1adb38700f444fb4bfd1f8d9c03668d
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-01-08 12:52:23 -0500

    Adds version service to test builds

---
+++ src/main/java/com/vmware/cloud/tanzu/bookservice/FooController.java
@@ -0,0 +1,13 @@
+package com.vmware.cloud.tanzu.bookservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @GetMapping("/version")
+    Long getVersion() {
+        return 1L;
+    }
+}
